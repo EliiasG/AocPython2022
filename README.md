12d65d56be67692ec1dbf23164e43700ffcb1345
eliiasg <egrundtvig@gmail.com>
2022-12-05 14:59:23 +0100

    solved day 5

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AocPython2022
-Solving Advent of Code 2022 in python 
+- Solving Advent of Code 2022 in python.
+- The python files should be run from the main directory.
---
+++ day5/solution1.py
@@ -0,0 +1,17 @@
+input1, input2 = open("day5\input.txt", "r").read().split("\n\n")
+
+input1 = list(reversed(input1.split("\n")))
+l = len(input1.pop(0).replace(" ", ""))
+stacks = [[] for i in range(l)]
+for line in input1:
+    ln = line[1::4]
+    for i, char in enumerate(ln):
+        if char != " ":
+            stacks[i].append(char)
+
+for line in input2.split("\n"):
+    amt, frm, to = [int(n) for n in line.split(" ") if n.isnumeric()]
+    for i in range(amt):
+        stacks[to-1].append(stacks[frm-1].pop())
+
+print("".join([stack[-1] for stack in stacks]))
