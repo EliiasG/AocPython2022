c52173ce1c833e4d33434706b3e66fdf73b0f9a0
eliiasg <egrundtvig@gmail.com>
2022-12-07 15:46:20 +0100

    solved day 7

---
+++ day7/solution1.py
@@ -0,0 +1,23 @@
+input = open("day7\input.txt", "r").read().split("\n")
+
+curdir = ""
+sizes = {}
+
+for line in input:
+    split = line.split(" ")
+    if split[0] == "$":
+        if split[1] == "cd":
+            if split[2] == "..":
+                oldSize = sizes[curdir]
+                curdir = "/".join(curdir.split("/")[:-2]) + "/"
+                sizes[curdir] += oldSize
+            else:
+                curdir += split[2].replace("/","") + "/"
+                if curdir not in sizes:
+                    sizes[curdir] = 0
+                #print(curdir)
+                
+    elif split[0] != "dir":
+        sizes[curdir] += int(split[0])
+
+print(sum([i for i in sizes.values() if i <= 100000]))
